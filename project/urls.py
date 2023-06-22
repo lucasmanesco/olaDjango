e0feb711b36247de9064728101d1a997e54a3b88
lucasmanesco <lucas_manesco@hotmail.com>
2023-06-21 22:44:09 -0300

    estudos-21-06

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+# HTTP Request <-> HTTP Response
+# MVT - Model View Controller (variação de MVC)
+
+
+def home(request):
+    print('home')
+    return HttpResponse('home')
+
+
+def blog(request):
+    print('blog')
+    return HttpResponse('blog')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', blog),
+    path('', home),
 ]
